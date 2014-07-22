5662f46dd85c5b99829c6586c744885b84c1dae2
William Huang <wwhuang@umich.edu>
2014-07-21 23:25:12 -0400

    fixed SPI_FLUSH. current implementation waits for rx buffer to fill first. Thus, if we call SPI_FLUSH to make sure the rx fifo is empty when the rx fifo is already empty, we enter an infinite loop

--- cpu/cc2538/spi-arch.h
+++ cpu/cc2538/spi-arch.h
@@ -56,6 +56,7 @@
 } while(0)
 
 #define SPI_WAITFOREORx() do { \
+  while(REG(SSI0_BASE + SSI_SR) & SSI_SR_BSY); \
   while(!(REG(SSI0_BASE + SSI_SR) & SSI_SR_RNE)); \
 } while(0)
 
@@ -63,7 +64,6 @@
 #error "You must include spi-arch.h before spi.h for the CC2538."
 #endif
 #define SPI_FLUSH() do { \
-  SPI_WAITFOREORx(); \
   while (REG(SSI0_BASE + SSI_SR) & SSI_SR_RNE) { \
     SPI_RXBUF; \
   } \
