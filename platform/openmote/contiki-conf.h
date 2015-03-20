0f28cdafc6fdc5b5b67e3e78ce5978bc34c58f26
Johan Bregell <johan_bregell@hotmail.com>
2015-03-20 09:05:23 +0100

    Changed back default openmote rf channel to 25

--- platform/openmote/contiki-conf.h
+++ platform/openmote/contiki-conf.h
@@ -398,7 +398,7 @@ typedef uint32_t rtimer_clock_t;
 #endif
 
 #ifndef CC2538_RF_CONF_CHANNEL
-#define CC2538_RF_CONF_CHANNEL              26
+#define CC2538_RF_CONF_CHANNEL              25
 #endif /* CC2538_RF_CONF_CHANNEL */
 
 #ifndef CC2538_RF_CONF_AUTOACK
